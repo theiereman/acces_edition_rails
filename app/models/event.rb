d8456563159113b77eb8c047b94c2ecd614aab47
theiereman <thomas.eyermann@protonmail.com>
2025-05-19 06:59:57 +0200

    feat: event validators

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, length: { minimum: 10 }
+  validates :location, presence: true
+  validates :date, future: true
+
+  validates :user, presence: true
 end
