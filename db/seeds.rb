90541b6bc0ae6f71f67439207064e179b2519433
theiereman <thomas.eyermann@protonmail.com>
2025-05-19 07:00:15 +0200

    chore: seeds to facilitate tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+5.times do |i|
+  u = User.create!(
+    email: "test#{i}@test.com",
+    password: "password"
+  )
+
+  p "created user #{u.email} with password 'password'"
+
+  10.times do |i|
+    Event.create!(
+      title: "Event #{i} from user #{u.email}",
+      description: "Description #{i}",
+      location: "Location #{i}",
+      date: Time.now + i.days,
+      user: u
+    )
+
+    p "created event #{i} for user #{u.email}"
+  end
+end
